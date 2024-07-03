18cad105e3ab452b86d7099364d99e83155290b3
Markus Hautala <71873935+markus-hautala@users.noreply.github.com>
2024-07-03 23:40:28 +0300

    forgot

--- paketin_lataus.sh
+++ paketin_lataus.sh
@@ -1,6 +1,7 @@
 !/bin/bash
 
 mkdir caspar_testiasennus_mha
+cd caspar_testiasennus_mha
 wget https://github.com/CasparCG/server/releases/download/v2.4.0-stable/casparcg-server-v2.4.0-stable-ubuntu22.zip
 sudo apt install unzip
 unzip casparcg-server-v2.4.0-stable-ubuntu22.zip
