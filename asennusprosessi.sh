f67af0d6f36f82a06a043b0baa3951b8c88ac9f7
Markus Hautala <71873935+markus-hautala@users.noreply.github.com>
2024-07-03 23:30:31 +0300

    added delays

--- asennusprosessi.sh
+++ asennusprosessi.sh
@@ -6,7 +6,9 @@ cd casparcg_server
 cp ../../casparcg.config casparcg.config
 
 startx
+sleep 10s
 xrandr --output HDMI-0 --mode 1920x1080 --rate 50.00
+sleep 5s
 xrandr --output HDMI-0 --below HDMI-1
 
 ./run.sh
