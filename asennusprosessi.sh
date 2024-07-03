ae7569af8ec649b1f391b2949b0747d6579c7134
Markus Hautala <71873935+markus-hautala@users.noreply.github.com>
2024-07-03 23:29:21 +0300

    poistettu siirretyt

--- asennusprosessi.sh
+++ asennusprosessi.sh
@@ -1,14 +1,7 @@
 !/bin/bash
 
-mkdir caspar_testiasennus_mha
 cd caspar_testiasennus_mha
-wget https://github.com/CasparCG/server/releases/download/v2.4.0-stable/casparcg-server-v2.4.0-stable-ubuntu22.zip
-sudo apt install unzip
-unzip casparcg-server-v2.4.0-stable-ubuntu22.zip
 cd casparcg_server
-
-echo "----------------"
-echo "Asennuspaketti ladattu. Suoritetaan asennus:"
 
 cp ../../casparcg.config casparcg.config
 
