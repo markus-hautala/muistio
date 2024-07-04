a06d6c9e56bcd873debbd2c82ba3f231ae883701
Markus Hautala <71873935+markus-hautala@users.noreply.github.com>
2024-07-04 19:37:04 +0300

    echo

--- 2_setUI.sh
+++ 2_setUI.sh
@@ -2,5 +2,5 @@
 
 #startx -- -nocursor # when server ubuntu
 
+echo "Piilotetaan hiiri. Avaa uusi terminal-tab seuraavaan komentoon"
 unclutter -idle 0 -jitter 10 # when desktop
-echo "Hiiri piilotettu. Avaa uusi terminal-tab seuraavaan komentoon"
