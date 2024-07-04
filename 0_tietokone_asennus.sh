228546b27df861e9ce64abce5dc4ec42b37354fd
Markus Hautala <71873935+markus-hautala@users.noreply.github.com>
2024-07-04 19:00:50 +0300

    cursor hide

--- 0_tietokone_asennus.sh
+++ 0_tietokone_asennus.sh
@@ -2,3 +2,4 @@
 
 sudo apt install nvidia-driver-535-server
 sudo apt install xorg
+sudo apt install unclutter
---
+++ 2_setUI.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#startx -- -nocursor # when server ubuntu
+uncluter -idle 0 # when desktop
