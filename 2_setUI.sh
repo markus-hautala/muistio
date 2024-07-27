8d4b38ead6a6bcde5669df6b01c69d59cf69774f
lutsi tekniikka <lutsi.tekniikka@gmail.com>
2024-07-27 18:36:07 +0300

    hiiren piilotus jitter pienemmäksi

--- 2_setUI.sh
+++ 2_setUI.sh
@@ -2,5 +2,5 @@
 
 #startx -- -nocursor # when server ubuntu
 
-echo "Piilotetaan hiiri. Avaa uusi terminal-tab seuraavaan komentoon"
-unclutter -idle 0 -jitter 10 # when desktop
+echo "Piilotetaan hiiri. Avaa uusi terminal-tab seuraavaan komentoon. Älä hammenny vaikka hiiri havisi  :)"
+unclutter -idle 0 -jitter 5 # when desktop ubuntu
